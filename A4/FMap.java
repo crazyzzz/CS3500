076848f7ebed8eb47cc8774b9659f8c236222e74
Kevin L <klanger@login-hk55fp1.ccs.neu.edu>
2013-05-15 20:17:48 -0400

    init

---
+++ A4/FMap.java
@@ -0,0 +1,12 @@
+/**
+ * Assignment 4 - FMap.
+ * @author Kevin Langer
+ * klanger@ccs.neu.edu
+ *
+ * An implementation of maps using abstract
+ * data types. Three classes: abstract class FMap
+ * and Empty and Add which serve as the basic
+ * creators for FMap.
+ */
+
+
